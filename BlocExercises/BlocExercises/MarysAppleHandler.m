50a8423aeee4e83a65cbafeb7666ec950d496ffb
razzyfrazzy <raizaab@me.com>
2015-03-27 10:47:23 -0700

    Completed exercise

--- BlocExercises/BlocExercises/MarysAppleHandler.m
+++ BlocExercises/BlocExercises/MarysAppleHandler.m
@@ -15,6 +15,26 @@
 
     /* WORK HERE */
     
+    if (dollars >=1000000000){
+        itemToReturn = @"have The Big Apple";
+    }
+    
+    else if (dollars >=1000){
+        itemToReturn = @"have an Apple computer";
+    }
+    
+    else if (dollars >=6){
+        itemToReturn = @"have an apple";
+    }
+    
+    else if (dollars >=5){
+        itemToReturn = @"have some gum";
+        
+    }
+    
+    else {
+        itemToReturn = @"get out of my store";
+    }
     
     NSLog(@"For $%ld, Mary can: %@", (long)dollars, itemToReturn);
     
@@ -30,7 +50,7 @@
         cost *= .75;
     }
     
-    return cost;
+    return self.getsDiscount ? 18 : 24;;
 }
 
 @end
